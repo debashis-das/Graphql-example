d3d82630a48e11fe0f3f9a7672ebd9a5fe6f7257
Debashis <debashis@Deepanshus-MacBook-Pro.local>
2020-06-23 14:18:28 +0530

    Deb | Node Server for GQL

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+//conenct to mongo
+
+mongoose.connect('mongodb+srv://debashis:yF2F7gZ5rSCV1FQC@cluster0-hjgzr.mongodb.net/graphql-test?retryWrites=true&w=majority');
+mongoose.connection.once('open',()=>{
+    console.log('connected to database');
+});
+
+app.use("/graphql",graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000,() => {
+    console.log('now listening for requests on port 4000');
+})
